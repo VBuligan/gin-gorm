8d14216b47f93e1260e389908d28c7e4dd4fbb1c
jankey <vasili.coder@gmail.com>
2023-04-05 21:06:34 +0300

    helpers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -17,14 +17,51 @@ func GetAllArticles(c *gin.Context) {
 }
 
 func PostNewArticles(c *gin.Context) {
-
+	var article models.Article
+	c.BindJSON(&article)
+	err := models.AddNewArticle(&article)
+	if err != nil {
+		helpers.ResponseJson(c, 404, article)
+		return
+	}
+	helpers.ResponseJson(c, 201, article)
 }
+
 func GetArticleById(c *gin.Context) {
-
+	id := c.Param.ByName("id")
+	var article models.Article
+	err := models.GetArticleById(&article, id)
+	if err != nil {
+		helpers.ResponseJson(c, 404, article)
+		return
+	}
+	helpers.ResponseJson(c, 200, article)
 }
+
 func UpdateArticleById(c *gin.Context) {
-
+	var article models.Article
+	id := c.Param.ByName()
+	err := models.GetArticleById(&article, id)
+	if err != nil {
+		helpers.ResponseJson(c, 404, article)
+		return
+	}
+	c.BindJSON(&article)
+	err = models.UpdateArticle(&article)
+	if err != nil {
+		helpers.ResponseJson(c, 404, article)
+		return
+	}
+	helpers.ResponseJson(c, 202, article)
 }
-func DeleteArticleById(c *gin.Context) {
 
+func DeleteArticleById(c *gin.Context) {
+	var article models.Article
+	id := c.Param.ByName("id")
+	err := models.DeleteById(&article, id)
+	if err != nil {
+		helpers.ResponseJson(c, 404, article)
+		return
+	}
+	helpers.ResponseJson(c, 202, article)
 }
