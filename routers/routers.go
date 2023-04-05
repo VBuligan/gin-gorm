d768eb985b798223dcf1fc9d794c9239a7b75e36
jankey <vasili.coder@gmail.com>
2023-04-05 20:20:19 +0300

    router group by path

--- routers/routers.go
+++ routers/routers.go
@@ -1,1 +1,15 @@
 package routers
+
+import "github.com/gin-gonic/gin"
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default() // * Аналог mux.NewRouter
+
+	// * В gin принято группировать ресурсы
+	apiV1Group := router.Group("/api/v1") // * prefix
+	apiV1Group.GET("article", handlers.GetAllArticles)
+	apiV1Group.POST("article", handlers.PostNewArticles)
+	apiV1Group.GET("article/:id", handlers.GetArticleById)
+	apiV1Group.PUT("article/:id", handlers.UpdateArticleById)
+	apiV1Group.DELETE("article/:id", handlers.DeleteArticleById)
+}
