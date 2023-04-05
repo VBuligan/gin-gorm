fa10dc81db96e69e6c95fb23fde61810cf15b8e3
jankey <vasili.coder@gmail.com>
2023-04-05 20:14:22 +0300

    routers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/VBuligan/gin-gorm/models"
 	"github.com/VBuligan/gin-gorm/storage"
 	"github.com/jinzhu/gorm"
 	"log"
@@ -16,5 +17,9 @@ func main() {
 	defer storage.DB.Close()
 
 	// * В этот момент орм сама сгенерит все запросы, миграции и применит их
-	storage.DB.AutoMigrate(&model.Article{})
+	storage.DB.AutoMigrate(&models.Article{})
+
+	// * r - gin маршрутизатор
+	r := routers.SetupRouters()
+	r.Run()
 }
--- models/scheme.go
+++ models/scheme.go
@@ -4,4 +4,13 @@ import "github.com/jinzhu/gorm"
 
 type Article struct {
 	gorm.Model
+	Title   string `json:"title"`
+	Author  string `json:"author"`
+	Content string `json:"content"`
+}
+
+// * Горм требует наличие метода возвращающего имя таблицы
+
+func (a *Article) TableName() string {
+	return "article"
 }
